d8105d937724dc0bba83e2968b25d14b901fbb8e
Suresh Bijja <sureshbijja@gmail.com>
2026-06-28 13:57:01 -0500

    fix: use (name, value) signature for req.cookies.set in middleware

--- middleware.ts
+++ middleware.ts
@@ -26,14 +26,14 @@ export async function middleware(req: NextRequest) {
               return req.cookies.get(name)?.value
             },
             set(name: string, value: string, options: CookieOptions) {
-              req.cookies.set({ name, value, ...options } as Parameters<typeof req.cookies.set>[0])
+              req.cookies.set(name, value)
               response = NextResponse.next({ request: { headers: req.headers } })
-              response.cookies.set({ name, value, ...options } as Parameters<typeof response.cookies.set>[0])
+              response.cookies.set(name, value, options)
             },
             remove(name: string, options: CookieOptions) {
-              req.cookies.set({ name, value: '', ...options } as Parameters<typeof req.cookies.set>[0])
+              req.cookies.set(name, '')
               response = NextResponse.next({ request: { headers: req.headers } })
-              response.cookies.set({ name, value: '', ...options } as Parameters<typeof response.cookies.set>[0])
+              response.cookies.set(name, '', options)
             },
           },
         }
